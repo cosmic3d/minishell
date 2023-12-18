ff17269b54e17df2f3c7fd71fbf47af9c70bd75c
apresas- <albertprpu97@gmail.com>
2023-12-18 17:02:31 +0100

    arreglos llamar_builtins

--- src/general/builtins/llamar_builtins.c
+++ src/general/builtins/llamar_builtins.c
@@ -6,7 +6,7 @@
 /*   By: apresas- <apresas-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 16:44:01 by apresas-          #+#    #+#             */
-/*   Updated: 2023/12/18 15:06:28 by apresas-         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:02:12 by apresas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,18 @@
 int	llamar_builtins(t_ms *ms)
 {
 	if (!ft_strcmp(ms->token->content, "exit"))
-	{
 		return (ms_exit(ms->cmd->args));
-	}
 	else if (!ft_strcmp(ms->cmd->args[0], "echo"))
-	{
 		return (ms_echo(ms->cmd->args));
-	}
 	else if (!ft_strcmp(ms->cmd->args[0], "env"))
-	{
 		return (ms_env(ms));
-	}
 	else if (!ft_strcmp(ms->cmd->args[0], "cd"))
-	{
 		return (ms_cd(ms, ms->cmd->args));
-	}
 	else if (!ft_strcmp(ms->cmd->args[0], "export"))
-	{
 		return (ms_export(ms, ms->cmd->args));
-	}
 	else if (!ft_strcmp(ms->cmd->args[0], "pwd"))
-	{
 		return (ms_pwd(ms));
-	}
 	else if (!ft_strcmp(ms->cmd->args[0], "unset"))
-	{
 		return (ms_unset(ms, ms->cmd->args));
-	}
-	return (-1); // debugging	
+	return (-1);
 }
